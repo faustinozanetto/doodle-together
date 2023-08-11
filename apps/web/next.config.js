057f89b1a9a3150da54dedf84144d541861a559b
Faustino Zanetto <zanettofaustino@gmail.com>
2023-08-10 23:34:27 -0300

    chore(web): updated nextjs config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,10 +1,4 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  output: 'standalone',
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, '../../'),
-  },
 };
